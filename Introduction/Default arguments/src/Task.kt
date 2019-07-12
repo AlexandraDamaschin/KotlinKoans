601c23582ec780106bdea6428fad041c748aef0f
Alexandra Damaschin <damaschin.alexandra.maria@gmail.com>
2019-07-12 09:51:23 +0300

    [4] Declare functions with default parameters

--- Introduction/Default arguments/src/Task.kt
+++ Introduction/Default arguments/src/Task.kt
@@ -1,9 +1,10 @@
-fun foo(name: String, number: Int, toUpperCase: Boolean) =
+fun foo(name: String, number: Int = 42, toUpperCase: Boolean = false) =
         (if (toUpperCase) name.toUpperCase() else name) + number
 
 fun useFoo() = listOf(
         foo("a"),
         foo("b", number = 1),
         foo("c", toUpperCase = true),
+        //Calling a function with names variables makes the code more readable
         foo(name = "d", number = 2, toUpperCase = true)
 )
