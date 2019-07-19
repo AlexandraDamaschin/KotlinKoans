9d56d0f697393b9321ef4a105d75e7de55e58a7a
Alexandra Damaschin <damaschin.alexandra.maria@gmail.com>
2019-07-19 11:09:09 +0300

    [18] Iterate through dates with next

--- Conventions/For loop/src/Task.kt
+++ Conventions/For loop/src/Task.kt
@@ -1,4 +1,19 @@
-class DateRange(val start: MyDate, val end: MyDate)
+// Date range class implements a iterable<MyDate>
+class DateRange(val start: MyDate, val end: MyDate): Iterable<MyDate>{
+    override fun iterator(): Iterator<MyDate> = DateIterator(this)
+}
+
+// Data Iterator implements a iterable <MyDate>
+class DateIterator(val dateRange:DateRange) : Iterator<MyDate> {
+    //current date is start date
+    var current: MyDate = dateRange.start
+    override fun next(): MyDate {
+        val result = current
+        current = current.nextDay()
+        return result
+    }
+    override fun hasNext(): Boolean = current <= dateRange.end
+}
 
 fun iterateOverDateRange(firstDate: MyDate, secondDate: MyDate, handler: (MyDate) -> Unit) {
     for (date in firstDate..secondDate) {
